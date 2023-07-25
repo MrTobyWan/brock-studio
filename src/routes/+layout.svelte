aee33ab5fcaa3857e89ab5b06e40b8de6406e311
MrTobyWan <tobiasbrock@gmail.com>
2023-07-25 14:52:08 +0200

    Started from scratch using Skeleton UI and Tailwind CSS. Wish me luck!

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
-	import { theme } from '$lib/stores';
+	// The ordering of these imports is critical to your app working properly
+	import '@skeletonlabs/skeleton/themes/theme-crimson.css';
+	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+	// Most of your app wide CSS should be put in this file
+	import '../app.postcss';
+
+	import { LightSwitch } from '@skeletonlabs/skeleton';
 </script>
 
-<svelte:head>
-	<meta name="color-scheme" content={$theme == 'system' ? 'light dark' : $theme} />
-	<link rel="stylesheet" href={`/theme/${$theme}.css`} />
-</svelte:head>
+<div class="static">
+	<nav class="absolute top-2 left-2">
+		<ul>
+			<li>Home</li>
+		</ul>
+	</nav>
+	<div class="absolute top-2 right-2">
+		<LightSwitch />
+	</div>
+</div>
 
 <slot />
