4248dbfbf8cb18e9fd698185bf72607fc3979ac1
MrTobyWan <tobiasbrock@gmail.com>
2023-07-25 15:39:28 +0200

    En "about side" placeholder...

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,6 @@
+<div class="container mx-auto flex justify-center items-center">
+	<div class="p-10">
+		<strong class="h1">Om mig</strong>
+	</div>
+</div>
+<p class="flex justify-center">Her skal der også stå en masse ting...</p>
